4f231416f24fd91bb4b7b8104be7380b7de0727b
vann-at-cpcp-dot-tw <vann@cpcp.tw>
2021-12-28 21:46:20 +0800

    新增 static 設定檔

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  target: process.argv.includes('generate') ?'static' :'server',
   buildModules: [
     ["@pinia/nuxt", { disableVuex: true }]
   ],
