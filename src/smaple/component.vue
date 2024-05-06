8906ce005336ea1bf81d8abda6463298928096ee
Vann Liu <vann@cpcp.tw>
2024-05-06 15:47:21 +0800

    feat: SFC defineProps by TS

--- src/smaple/component.vue
+++ src/smaple/component.vue
@@ -1,18 +1,11 @@
 <script lang="tsx" setup>
 import { twMerge } from 'tailwind-merge'
 const window = process.client ? globalThis : null
-
-const props = defineProps({
-  class: {
-    type: String,
-    default: '',
-  },
-  className: {
-    type: String,
-    default: '',
-  }
-})
-
+interface IProps {
+  class?: string
+  className?: string
+}
+const props = defineProps<IProps>()
 </script>
 <template>
   <div :class="twMerge('', props.class)"></div>
